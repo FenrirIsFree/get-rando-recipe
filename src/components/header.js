a81802ac48219bf470410ccd5c512b5cdf991d8a
FenrirIsFree <nickscape96@gmail.com>
2023-03-14 15:01:26 -0400

    Initial Commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line arrow-body-style
+const Header = () => {
+  return (
+    <div>
+      <div className="jumbotron text-center">
+        <div className="container">
+          <h1 className="jumbotron-heading">Pick A Random Recipe</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
